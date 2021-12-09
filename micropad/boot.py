3927d4b4eca18c7883384a091944568f0c076eb1
billash <wrash0806@my.mwsu.edu>
2021-12-08 23:04:50 -0500

    Update startup

--- micropad/boot.py
+++ micropad/boot.py
@@ -1,3 +1,25 @@
+import board 
+import digitalio
+import storage
 import supervisor
 
+# Rename board 
+# https://learn.adafruit.com/welcome-to-circuitpython/renaming-circuitpy
+ 
+ # disable usb on startup 
+try:
+    button = digitalio.DigitalInOut(board.GP15)
+    storage.disable_usb_drive()
+except: 
+    storage.enable_usb_drive() 
+
+# if storage.getmount('/').label == 'CIRCUITPY':
+#     m = storage.getmount("/", readonly=False)
+#     m.label = "ZOOM_BOARD"
+#     storage.remount("/", readonly=False)
+# # storage.enable_usb_drive()
+
 supervisor.set_next_stack_limit(4096 + 4096)
+
+# disable repl 
+# usb_cdc.disable()  
